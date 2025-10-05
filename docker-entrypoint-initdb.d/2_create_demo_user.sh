50413a7a231e72f4a6cec2122b99568324635666
akryll <admin@ideyniy.ru>
2025-10-06 00:26:00 +0300

    fix rights

--- docker-entrypoint-initdb.d/2_create_demo_user.sh
+++ docker-entrypoint-initdb.d/2_create_demo_user.sh
@@ -5,13 +5,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	CREATE USER demo WITH PASSWORD '$DEMO_USER_PASS';
 	GRANT ALL PRIVILEGES ON DATABASE demo TO demo;
 	\c demo
-	CREATE SCHEMA bookings;
 	-- Дать право на использование схемы
-	GRANT USAGE ON SCHEMA bookings TO demo;
+	ALTER DATABASE demo OWNER TO demo;
+	ALTER SCHEMA bookings OWNER TO demo;
+	GRANT pg_write_all_data TO demo;
+	GRANT pg_read_all_data TO demo;
 
-	-- Дать все права на все таблицы в схеме
-	GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA bookings TO demo;
-
-	-- Дать права на выполнение всех функций
-	GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA bookings TO demo;
 EOSQL
